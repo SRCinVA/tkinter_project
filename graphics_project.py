8a7ba6779118c508f3685a1a6aee2aabae8d508c
SRCinVA <scitarella@hotmail.com>
2021-12-12 11:29:54 -0500

    started on basic window
    Already had three differences for iOS from Windows, but solved them.

--- graphics_project.py
+++ graphics_project.py
@@ -1,9 +1,21 @@
 from tkinter import *
 
+# for iOS, have to put the function first
+def km_to_miles():
+    print("Success!") # printing out a statment helps you highlight where problems are happening
+
 window = Tk()
 
-b1=Button(window, text="Execute")
-b1.pack()
+b1=Button(window, text="Execute", command=km_to_miles()) # .. AND you need the brackets for iOS.
+b1.grid(row=0,column=0, rowspan=3)
+
+# an entry widget:
+e1=Entry(window)
+e1.grid(row=0,column=1)
+
+# one for text (with parameters for dimensions, and for iOS, to highlight the text box):
+t1=Text(window, height=1, width=20, bd=1, relief='sunken')
+t1.grid(row=0,column=2)
 
 # should be at the end.
 window.mainloop()
